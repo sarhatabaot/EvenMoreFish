d9cc748ebd5ce41dde752581705c55c36c03abeb
FireML <fireml@firedev.uk>
2025-04-24 09:04:03 +0100

    Fix addons in dev builds (#635)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,13 @@
 rootProject.name = "even-more-fish"
 
-include(":even-more-fish-api")
-include(":even-more-fish-database-extras")
-include(":even-more-fish-plugin")
-
 // Addons
 include(":addons:even-more-fish-addons-j17")
 include(":addons:even-more-fish-addons-j21")
+
+// Plugin Stuff
+include(":even-more-fish-api")
+include(":even-more-fish-database-extras")
+include(":even-more-fish-plugin")
 
 dependencyResolutionManagement {
     versionCatalogs {
